5d2a1f3374147945fcbe4888ef0d61d93d636cf4
Pausrq27 <masanchezro@est.utn.ac.cr>
2025-11-27 19:35:27 -0600

    fixing conflicts

--- app/src/main/java/cr/ac/utn/movil/MainActivity.kt
+++ app/src/main/java/cr/ac/utn/movil/MainActivity.kt
@@ -1,6 +1,8 @@
 package cr.ac.utn.movil
-import android.content.Intent
+
 import cr.ac.utn.movil.activities.PayenInsuranceListActivity
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
